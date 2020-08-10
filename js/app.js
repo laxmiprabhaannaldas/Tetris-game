e5453a4af0c89b76eabbafd1cd7d08fc7cbd4986
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-10 15:55:55 +0530

    Added the freeze function to freeze the tetrominoes

--- js/app.js
+++ js/app.js
@@ -143,7 +143,21 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
 
+        //freeze the shape
+        function freeze() {
 
+            if (current.some(index => squares[currentPosition + index + width].classList.contains('block3') ||
+                    squares[currentPosition + index + width].classList.contains('block2'))) {
+                current.forEach(index => squares[index + currentPosition].classList.add('block2'))
+
+                random = nextRandom
+                nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+                current = theTetrominoes[random][currentRotation]
+                currentPosition = 4
+                draw()
+                displayShape()
+            }
+        }
 
     }
 
