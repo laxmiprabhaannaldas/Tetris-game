3cafff66570840dc79ffdaaf5d4739b939b07892
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-10 14:28:27 +0530

    Added the random selection function and methods related to moving of object

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
         const grid = document.querySelector('.grid')
+        let squares = Array.from(grid.querySelectorAll('div'))
         const width = 10
         const height = 20
+        let currentPosition = 4
         const lTetromino = [
             [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, 2],
             [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
@@ -38,6 +40,54 @@ document.addEventListener('DOMContentLoaded', () => {
         ]
         const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
+        //radomly select Tetromino
+        let random = Math.floor(Math.random() * theTetrominoes.length)
+        let currentRotation = 0
+        let current = theTetrominoes[random][currentRotation]
+
+        //draw the shape
+        function draw() {
+            current.forEach(index => (
+                squares[currentPosition + index].classList.add('block')
+            ))
+        }
+
+        //undraw the shape
+        function undraw() {
+            current.forEach(index => {
+                squares[currentPosition + index].classList.remove('block')
+            })
+        }
+
+        //move down shape
+        function moveDown() {
+            undraw()
+            currentPosition = currentPosition += width
+            draw()
+            freeze()
+        }
+
+        //move left and prevent collisions with shapes moving left
+        function moveRight() {
+            undraw()
+            const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+            if (!isAtRightEdge) currentPosition += 1
+            if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+                currentPosition -= 1
+            }
+            draw()
+        }
+
+        //move right and prevent collisions with shapes moving right
+        function moveleft() {
+            undraw()
+            const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+            if (!isAtLeftEdge) currentPosition -= 1
+            if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+                currentPosition += 1
+            }
+            draw()
+        }
     }
 
 )
