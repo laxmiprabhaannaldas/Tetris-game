8876b94867a085480204c1534b55114def591983
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-10 17:23:20 +0530

    developed the code of final output i.e Showing Score,Lines,Game over to user

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+        const startBtn = document.querySelector('button')
         const grid = document.querySelector('.grid')
+        const scoreDisplay = document.querySelector('.score-display')
+        const lineDisplay = document.querySelector('.line-display')
         const displaySquares = document.querySelectorAll('.previous-grid div')
         let squares = Array.from(grid.querySelectorAll('div'))
         const width = 10
         const height = 20
         let currentPosition = 4
+        let timerId
         let nextRandom = 0
+        let score = 0
+        let lines = 0
+        let currentIndex = 0
 
         //assign functions to keycodes
         function control(e) {
@@ -156,6 +162,49 @@ document.addEventListener('DOMContentLoaded', () => {
                 currentPosition = 4
                 draw()
                 displayShape()
+                gameOver()
+                addScore()
+            }
+        }
+
+        startBtn.addEventListener('click', () => {
+            if (timerId) {
+                clearInterval(timerId)
+                timerId = null
+            } else {
+                draw()
+                timerId = setInterval(moveDown, 1000)
+                nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+                displayShape()
+            }
+        })
+
+        //game over
+        function gameOver() {
+            if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+                scoreDisplay.innerHTML = 'end'
+                clearInterval(timerId)
+            }
+        }
+
+        //add score
+        function addScore() {
+            for (currentIndex = 0; currentIndex < 199; currentIndex += width) {
+                const row = [currentIndex, currentIndex + 1, currentIndex + 2, currentIndex + 3, currentIndex + 4, currentIndex + 5, currentIndex + 6, currentIndex + 7, currentIndex + 8, currentIndex + 9]
+                if (row.every(index => squares[index].classList.contains('block2'))) {
+                    score += 10
+                    lines += 1
+                    scoreDisplay.innerHTML = score
+                    lineDisplay.innerHTML = lines
+                    row.forEach(index => {
+                        squares[index].classList.remove('block2') || squares[index].classList.remove('block')
+                    })
+
+                    //splice array
+                    const squareRemoved = squares.splice(currentIndex, width)
+                    squares = squareRemoved.concat(squares)
+                    squares.forEach(cell => grid.appendChild(cell))
+                }
             }
         }
 
